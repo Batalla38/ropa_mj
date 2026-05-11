0c3390f0a6fa2e894562aa9daa0fadfe1e17d971
Melu <solangemtl88@gmail.com>
2026-05-11 20:51:20 -0300

    conBASE

---
+++ database/migrations/2026_05_11_233720_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+        {
+                Schema::create('productos', function (Blueprint $table) {
+                $table->id();
+                $table->string('nombre', 150);
+                $table->text('descripcion')->nullable();
+                $table->decimal('precio', 10, 2);
+                $table->integer('stock')->default(0);
+                $table->string('url_imagen')->nullable();
+                $table->boolean('activo')->default(true);
+                $table->timestamps();
+                });
+        }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
